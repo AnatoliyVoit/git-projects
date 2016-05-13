7890fa2b63b622843d28770fa1ea9e14da0ea187
Anatoliy <knite2332@gmail.com>
2016-05-13 15:16:17 -0300

    dictionary

--- Exercises/src/Exercise2_1/Exercise2_1.java
+++ Exercises/src/Exercise2_1/Exercise2_1.java
@@ -6,39 +6,32 @@ import java.util.ListIterator;
 import java.util.Random;
 
 public class Exercise2_1 {
-	public static void main (String[] args){
+	public static void main(String[] args) {
 		Random random = new Random();
-		
+
 		List<Integer> marksList = new ArrayList<>();
 		ListIterator<Integer> iterator = marksList.listIterator();
 		System.out.println("Список оценок:");
 		for (int i = 0; i < 10; i++) {
-			int x = random.nextInt(10)+1;
-			iterator.add(x);	
-			System.out.print(marksList.get(i)+ "\t");			
+			int x = random.nextInt(10) + 1;
+			iterator.add(x);
+			System.out.print(marksList.get(i) + "\t");
 		}
-		
+
 		System.out.println("\nСписок положительных оценок:");
 		/*
-		for (int i = 0; i < marksList.size(); i++) {
-			marksList.get(i);			
-			if (marksList.get(i)<5){				
-				marksList.remove(i);
-				i--;
-				System.out.print("-" + "\t");
-			}else {
-				System.out.print(marksList.get(i)+ "\t");
-			}			
-		}
-		*/
+		 * for (int i = 0; i < marksList.size(); i++) { marksList.get(i); if
+		 * (marksList.get(i)<5){ marksList.remove(i); i--; System.out.print("-"
+		 * + "\t"); }else { System.out.print(marksList.get(i)+ "\t"); } }
+		 */
 		iterator = marksList.listIterator();
-		while (iterator.hasNext()){
+		while (iterator.hasNext()) {
 			int x = iterator.next();
-			if (x<5){
+			if (x < 5) {
 				iterator.remove();
 				System.out.print("-" + "\t");
 			} else {
-				System.out.print(x+ "\t");
+				System.out.print(x + "\t");
 			}
 		}
 
---
+++ Exercises/src/Exersice2_3/Exersice2_3.java
@@ -0,0 +1,34 @@
+package Exersice2_3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.sun.org.apache.xpath.internal.operations.Quo;
+
+public class Exersice2_3 {
+
+	public static void main(String[] args) {
+		String text = " омически передал слезливую речь предводител€ и как выписаны были лакеи в деревне. "
+				+ "—одержани€: неведовский со своим юным, непоколебимым и потому все честное. ѕростые, ровные ко всем отношени€, очень весело. "
+				+ "¬ыписаны были лакеи в которой все его превосходительству придетс€. ”важение и €довитым лицом кучу ни к неведовскому: лучше нельз€ было. "
+				+ "ƒвенадцатью шарами обедом и землевладельца, которые он сказал, сто€ло. ќтличным обедом перебирались эпизоды выборов так забрало за прекрасным.";
+		text = text.toLowerCase();
+		String[] array = text.split("\\s\"|\"\\s|\\.\\s|\\.|\\s|,\\s|:\\s");
+		int quontity = 1;
+		Set<String> iterator = new HashSet<String>();
+		Map<String, Integer> dictionary = new HashMap<String, Integer>();
+		for (int i = 0; i < array.length; i++) {
+//			System.out.println(array[i]);
+			if (!iterator.add(array[i])){
+				quontity++;
+			}
+			System.out.println(dictionary.hashCode());
+		}
+//		Set<String> list = new HashSet<String>(Arrays.asList(array));
+		
+	}
+
+}
