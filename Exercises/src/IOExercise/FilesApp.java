093c11ba0daf72fdba855e96781300f240e21d3a
Anatoliy <knite2332@gmail.com>
2016-05-18 14:56:58 -0300

    18.05.2016 IOExercise

---
+++ Exercises/src/IOExercise/FilesApp.java
@@ -0,0 +1,51 @@
+package IOExercise;
+import java.io.*;
+ 
+public class FilesApp {
+ 
+    public static void main(String[] args) {
+    	// создадим новый файл
+        File newFile = new File("D:\\Students\\Anatoliy\\git\\git-projects\\Exercises\\src\\IOExercise\\notes3.txt");
+        try
+        {
+            boolean created = newFile.createNewFile();
+            if(created)
+                System.out.println("Файл создан");
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        }
+        
+        try(FileWriter writer = new FileWriter("D:\\Students\\Anatoliy\\git\\git-projects\\Exercises\\src\\IOExercise\\notes3.txt", false))
+        {
+           // запись всей строки
+            String text = "Мама мыла раму, раму мыла мама";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+             
+            writer.flush();
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        } 
+        try(FileWriter writer = new FileWriter("D:\\Students\\Anatoliy\\git\\git-projects\\Exercises\\src\\IOExercise\\notes3.txt", true))
+        {
+           // запись всей строки
+            String text = "Мама мыла раму, раму мыла мама";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+             
+            writer.flush();
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        } 
+    } 
+}
