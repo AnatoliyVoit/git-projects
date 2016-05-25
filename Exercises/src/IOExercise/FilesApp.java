c9ccf0bd40ad3a9c5dda36a1c193163ac2a35907
Anatoliy <knite2332@gmail.com>
2016-05-25 15:10:10 -0300

    25.05.2016 io generator

--- Exercises/src/IOExercise/FilesApp.java
+++ Exercises/src/IOExercise/FilesApp.java
@@ -1,7 +1,78 @@
 package IOExercise;
 import java.io.*;
+import java.util.Random;
  
 public class FilesApp {
+	static int generateRandomNumber(){
+		Random random = new Random();
+		int number = random.nextInt();
+		return number;
+	}
+	
+	static int generateRandomNumber(int range){
+		Random random = new Random();
+		int number = random.nextInt(range);
+		return number;
+	}
+	
+	static char[] makeSymbolsArray() {
+		String letters = "abcdefghijklmnopqrstuvwxyz";
+	    String numbers = "0123456789";
+	    char[] symbols = new char[2*letters.length() + numbers.length()];
+	    int i = 0;
+	    for (char letter : letters.toCharArray()) {
+	        symbols[i] = letter;
+	        i++;
+	    }
+	    for (char letter : letters.toUpperCase().toCharArray()) {
+	        symbols[i] = letter;
+	        i++;
+	    }
+	    for (char number : numbers.toCharArray()) {
+	        symbols[i] = number;
+	        i++;
+	    }
+	    return symbols;
+	}
+	
+//	static String generateType(){
+//		int type = generateRandomNumber(3);
+//		String s = null;
+//		if (type == 0){
+//			s = generateDouble();
+//		}
+//		if (type == 1){
+//			s = generateInt();
+//		}
+//		if (type == 3){
+//			s = generateEmail();
+//		}		
+//		return s;
+//	}
+	
+	static String generateDouble(){
+		double number = Math.random()*100;
+		String s = Double.toString(number);
+		return s;
+	}
+	
+	static String generateInt(){
+		int number = generateRandomNumber();
+		String s = Integer.toString(number);
+		return s;
+	}
+	
+	static String generateEmail(){
+		char[] symbols = makeSymbolsArray();
+		int length = generateRandomNumber(7)+4;
+		char[] account = new char[length];
+		for (int i = 0; i < account.length; i++) {
+			account[i] = symbols[generateRandomNumber(symbols.length-1)];
+		}
+		String[] direct = new String [] {"mail.ru", "gmail.com", "tut.by", "yahoo.com", "bk.ru"};
+		String email = account.toString() + "@" + direct[generateRandomNumber(direct.length-1)];
+		return email;
+	}
  
     public static void main(String[] args) {
     	// создадим новый файл
@@ -15,7 +86,7 @@ public class FilesApp {
         catch(IOException ex){
              
             System.out.println(ex.getMessage());
-        }
+        }        
         
         try(FileWriter writer = new FileWriter("D:\\Students\\Anatoliy\\git\\git-projects\\Exercises\\src\\IOExercise\\notes3.txt", false))
         {
@@ -32,20 +103,24 @@ public class FilesApp {
              
             System.out.println(ex.getMessage());
         } 
-        try(FileWriter writer = new FileWriter("D:\\Students\\Anatoliy\\git\\git-projects\\Exercises\\src\\IOExercise\\notes3.txt", true))
-        {
-           // запись всей строки
-            String text = "Мама мыла раму, раму мыла мама";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append("knite2332@gmail.com");
-             
-            writer.flush();
-        }
-        catch(IOException ex){
-             
-            System.out.println(ex.getMessage());
-        } 
+        
+        for (int i = 0; i < 50; i++) {			
+	        try(FileWriter writer = new FileWriter("D:\\Students\\Anatoliy\\git\\git-projects\\Exercises\\src\\IOExercise\\notes3.txt", true))
+	        {
+	        	
+	           // запись всей строки
+	        	String s = generateEmail();	
+	            writer.write(s);
+	            System.out.println(s);
+	            // запись по символам
+	            writer.append(' ');
+	             
+	            writer.flush();
+	        }
+	        catch(IOException ex){
+	             
+	            System.out.println(ex.getMessage());
+	        } 
+        }    
     } 
 }
